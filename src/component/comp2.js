e08b7ab280768c2d59d494d53f1c6a583f89b1ff
Dantes <dantes_i@yahoo.com>
2024-07-27 18:35:21 +0300

    final commit

--- src/component/comp2.js
+++ src/component/comp2.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import EmployeeService from '../service/EmployeeService';
 
-<<<<<<< HEAD
 // something to follow
-=======
 
 //july 2024
->>>>>>> feature-1
